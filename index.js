53ce69dacf054cd3be0f1891a1fd15561bfc55af
andriesdeklerck <andriesdeklerck@hotmail.com>
2023-01-18 22:30:12 +0100

    Initial version

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const PrivatedataContract = require('./lib/privatedata-contract');
+
+module.exports.PrivatedataContract = PrivatedataContract;
+module.exports.contracts = [ PrivatedataContract ];
